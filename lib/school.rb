466d5dc961628b9e69532047e5013ab4d8a5dfa5
Greem <orangenewyorkcreativeagency@gmail.com>
2020-05-13 22:59:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,11 +3,9 @@
 class School
   attr_accessor :school_name, :roster
 
-  def initialize(school_name, roster)
-
+  def initialize(school_name)
     @school_name = school_name
     @roster = {}
-
   end
 
   def add_student(student_name, class_level)
