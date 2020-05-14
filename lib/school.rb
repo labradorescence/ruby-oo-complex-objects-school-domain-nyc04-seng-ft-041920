6246065588fa8a72d6971a2ef08d24f8ee00f578
Greem <orangenewyorkcreativeagency@gmail.com>
2020-05-14 01:04:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -189,8 +189,7 @@
     end
 
     def add_student(kid, level)
-      if roster[level] = nil || false
-        roster[level] = [] #add empty arr if key is empty
+      if roster[level] ||= [] #add empty arr if key is empty
       else
         roster[level] << kid
       end
